88c577e3f2d818422cf752b0c6b50a5b2393ebbc
Naba Rizvi <righteouschica2@gmail.com>
2022-04-08 16:12:16 -0700

    fixed logos

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var data = [
 {
-        "icon": "chi22.PNG",
+        "icon": "chi22.png",
         "alt": "logo for CHI conference",
         "title": "First-author Workshop Paper",
         "date": "Spring 2022",
         "desc": "Our paper on designing technologies to support clinicians in addressing implicit bias in healthcare was published at the Designing Ecosystems to Support the Management of Complex Health Needs Workshop!"
     },
 {
-        "icon": "chi22.PNG",
+        "icon": "chi22.png",
         "alt": "logo for CHI conference",
         "title": "First-author Workshop",
         "date": "Spring 2022",
         "desc": "<a href=https://qtbipoc-pd.org/'>Our workshop</a> on Queer, Trans, Black, Indigenous, People of Color will happen on May 1 at CHI."
     },
 {
-        "icon": "chi22.PNG",
+        "icon": "chi22.png",
         "alt": "logo for CHI conference",
         "title": "Late-Breaking Paper",
         "date": "Spring 2022",
